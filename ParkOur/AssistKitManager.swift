f126f7751a1a16b39200ab83b62a58f413fc8ad6
Yixin Bao <mint.mr@gmail.com>
2019-11-21 15:24:56 -0800

    AssistKit Validator

--- ParkOur/AssistKitManager.swift
+++ ParkOur/AssistKitManager.swift
@@ -11,6 +11,7 @@ import CoreBluetooth
 
 protocol AssistKitManagerDelegate: class {
     
+    func assistKitManagerDidPowerOn(assistKitManager: AssistKitManager)
     func didDiscoverAssistKit(assistKitManager: AssistKitManager)
     func didConnectToAssistKit(assistKitManager: AssistKitManager)
     func didDisconnectWithAssistKit(assistKitManager: AssistKitManager)
@@ -27,7 +28,13 @@ class AssistKitManager: NSObject {
     
     weak var delegate: AssistKitManagerDelegate?
     
+    override init() {
+        super.init()
+        centralManager.delegate = self
+    }
+    
     private let assistKitServiceUUID = CBUUID(string: "980C2C3A-089F-46FE-9177-5DE28ADBCE06")
+    private let characteristicUUID = CBUUID(string: "ec0e")
     
     static func isAlreadyPairedBefore() -> Bool {
         if UserDefaults.standard.string(forKey: Meta.pairedAssistKitKey) != nil {
@@ -45,6 +52,7 @@ extension AssistKitManager: CBCentralManagerDelegate {
         switch central.state {
         case .poweredOn:
             // Start discovering
+            self.delegate?.assistKitManagerDidPowerOn(assistKitManager: self)
             self.centralManager.scanForPeripherals(withServices: [assistKitServiceUUID], options: nil)
             print("holy")
         default:
@@ -55,15 +63,48 @@ extension AssistKitManager: CBCentralManagerDelegate {
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         print("found you")
         print(peripheral.name ?? "no name")
+        connectedPeripheral = peripheral
+        connectedPeripheral.delegate = self
+        self.centralManager.stopScan()
+        self.centralManager.connect(peripheral, options: nil)
         self.delegate?.didDiscoverAssistKit(assistKitManager: self)
+        
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         self.delegate?.didConnectToAssistKit(assistKitManager: self)
+        peripheral.discoverServices([assistKitServiceUUID])
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("fuck")
     }
     
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         self.delegate?.didDisconnectWithAssistKit(assistKitManager: self)
     }
     
+    func queryACharacteristic() {
+//        connectedPeripheral.readValue(for: [characteristicUUID])
+//        connectedPeripheral.readValue(for: connected)
+    }
+    
+    
+}
+
+extension AssistKitManager: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print(peripheral.services)
+        peripheral.discoverCharacteristics([characteristicUUID], for: peripheral.services![0])
+    }
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        print("found characteristic")
+        print(service.characteristics)
+        
+        peripheral.readValue(for: service.characteristics![0])
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        print(String(data: characteristic.value!, encoding: .utf8))
+    }
 }
--- ParkOur/MainViewController.swift
+++ ParkOur/MainViewController.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 import MapKit
+import SnapKit
 
 class MainViewController: UIViewController {
     
-    private let assistKitManager = AssistKitManager()
-
+    private var assistKitManager: AssistKitManager!
+    var dot: UIView!
+    var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        assistKitManager = AssistKitManager()
+        assistKitManager.delegate = self
         
+        view.backgroundColor = .white
         let mapView = MKMapView(frame: view.frame)
         view.addSubview(mapView)
         
         mapView.delegate = self
 
+        let assistkitIndicator = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+        assistkitIndicator.backgroundColor = .white
+        
+        assistkitIndicator.layer.cornerRadius = 12
+        view.addSubview(assistkitIndicator)
+        assistkitIndicator.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(100)
+        }
+        
+        
+        dot = UIView(frame: CGRect(x: 20, y: 20, width: 20, height: 20))
+        dot.layer.cornerRadius = 10
+        dot.backgroundColor = .gray
+        label = UILabel(frame: CGRect(x: 20, y: 50, width: 280, height: 40))
+        label.text = "initialized"
+        label.textColor = .black
+        assistkitIndicator.addSubview(dot)
+        
+        assistkitIndicator.addSubview(label)
+        
         
         // Do any additional setup after loading the view.
     }
@@ -29,12 +58,19 @@ class MainViewController: UIViewController {
 }
 
 extension MainViewController: AssistKitManagerDelegate {
+    func assistKitManagerDidPowerOn(assistKitManager: AssistKitManager) {
+        label.text = "Powered On"
+    }
+    
+    
     func didDiscoverAssistKit(assistKitManager: AssistKitManager) {
-        
+        dot.backgroundColor = .yellow
+        label.text = "Discovered AssistKit"
     }
     
     func didConnectToAssistKit(assistKitManager: AssistKitManager) {
-        
+        dot.backgroundColor = .green
+        label.text = "Connected to AssistKit"
     }
     
     func didDisconnectWithAssistKit(assistKitManager: AssistKitManager) {
